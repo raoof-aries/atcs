e411054c5cab8c58017c85af14862b2b4cb333b7
nandhuaries <nandhu.s@ariesesolutions.com>
2025-08-08 18:23:44 +0530

    files added

---
+++ drill-pipe-inspection.php
@@ -0,0 +1,179 @@
+<?php include('header.php'); ?>
+<section class="wrapper image-wrapper bg-overlay bg-overlay-400 text-white innerPageBannerContainer">
+    <div class="container pt-17 pb-17 pt-md-17 pb-md-17 text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h1 class="display-1 mb-3 text-white">Drill Pipe Straightening Service</h1>
+                <nav class="d-inline-block" aria-label="breadcrumb">
+                    <ol class="breadcrumb text-white">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Drill Pipe Straightening Service</li>
+                    </ol>
+                </nav>
+                <!-- /nav -->
+            </div>
+            <!-- /column -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+
+
+<section>
+    <div class="container">
+        <div class="row gx-lg-8 gy-8 mt-5 mt-md-12 mt-lg-0 mb-15 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="row gx-md-5 gy-5">
+                    <div class="col-md-4 offset-md-2 align-self-end">
+                        <figure class="rounded"><img src="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/top-left.jpg" srcset="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/top-left.jpg 2x" alt=""></figure>
+                    </div>
+                    <!--/column -->
+                    <div class="col-md-6 align-self-end">
+                        <figure class="rounded"><img src="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/top-right.jpg" srcset="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/top-right.jpg 2x" alt=""></figure>
+                    </div>
+                    <!--/column -->
+                    <div class="col-md-6 offset-md-1">
+                        <figure class="rounded"><img src="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/bottom-left.jpg" srcset="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/bottom-left.jpg 2x" alt=""></figure>
+                    </div>
+                    <!--/column -->
+                    <div class="col-md-4 align-self-start">
+                        <figure class="rounded"><img src="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/bottom-right.jpg" srcset="assets/esol/img/service-images/OCTG-maintenance/drill-pipe-straightening/bottom-right.jpg 2x" alt=""></figure>
+                    </div>
+                    <!--/column -->
+                </div>
+                <!--/.row -->
+            </div>
+            <!--/column -->
+            <div class="col-lg-6 serviceTextContainer">
+                <h2 class="display-4 mb-3">
+                    Drill Pipe Straightening Service
+                </h2>
+                <p class="lead mb-8 pe-xxl-2">
+                    Our drill pipe straightening service revitalizes bent or damaged pipes, ensuring enhanced performance and longevity. We utilize precision equipment and skilled technicians to restore your pipes to their original specifications.
+                </p>
+            </div>
+            <!--/column -->
+        </div>
+    </div>
+</section>
+
+
+<section class="wrapper bg-soft-primary">
+    <div class="container py-14 py-md-12">
+        <h2 class="display-4 mb-5 text-center pb-3">
+            Drill Pipe Inspection Services Include
+        </h2>
+        <div class="row gx-md-5 gy-5 mb-14 mb-md-17">
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-yellow h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>01</h2>
+                        <!-- <img src="assets/esol/icons/capability.png" class="svg-inject icon-svg icon-svg-md text-yellow mb-3" alt="" /> -->
+                        <h4>
+                            Magnetic Particle Inspections on Tool Joints and Upsets
+                        </h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-green h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>02</h2>
+                        <h4>
+                            EMI Inspection
+                        </h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-orange h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>03</h2>
+                        <h4>
+                            Visual Inspection And Thread Gauging
+                        </h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-blue h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>04</h2>
+                        <h4>Dimensional Check</h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-yellow h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>05</h2>
+                        <h4>
+                            Refacing Services
+                        </h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-orange h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>06</h2>
+                        <h4>Straightening Services</h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom border-soft-blue h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>07</h2>
+                        <h4>Hydro Blasting Internal & External</h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+            <!--/column -->
+            <div class="col-md-6 col-xl-3">
+                <div class="card shadow-lg card-border-bottom softCoral h-100">
+                    <div class="card-body serviceCardCstm">
+                        <h2>08</h2>
+                        <h4>Cement Removal Services</h4>
+                    </div>
+                    <!--/.card-body -->
+                </div>
+                <!--/.card -->
+            </div>
+            <!--/column -->
+        </div>
+        <!--/.row -->
+
+    </div>
+    <!-- /.container -->
+</section>
+
+
+
+
+<?php include('footer.php'); ?>
--- structural-steel-repairs.php
+++ structural-steel-repairs.php
@@ -26,15 +26,15 @@
             <div class="col-lg-6 order-lg-2">
                 <div class="row gx-md-5 gy-5">
                     <div class="col-md-4 offset-md-2 align-self-end">
-                        <figure class="rounded"><img src="./assets/img/photos/g1.jpg" srcset="./assets/img/photos/g1@2x.jpg 2x" alt=""></figure>
+                        <figure class="rounded"><img src="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-top-left.png" srcset="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-top-left.png 2x" alt=""></figure>
                     </div>
                     <!--/column -->
                     <div class="col-md-6 align-self-end">
-                        <figure class="rounded"><img src="./assets/img/photos/g2.jpg" srcset="./assets/img/photos/g2@2x.jpg 2x" alt=""></figure>
+                        <figure class="rounded"><img src="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-top-right.png" srcset="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-top-right.png 2x" alt=""></figure>
                     </div>
                     <!--/column -->
                     <div class="col-md-6 offset-md-1">
-                        <figure class="rounded"><img src="./assets/img/photos/g3.jpg" srcset="./assets/img/photos/g3@2x.jpg 2x" alt=""></figure>
+                        <figure class="rounded"><img src="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-bottom-left.png" srcset="assets/esol/img/service-images/welding-fabrication/deployment-class-certified-welders/atcs-service-bottom-left.png 2x" alt=""></figure>
                     </div>
                     <!--/column -->
                     <div class="col-md-4 align-self-start">
