a57b36800487f8d3769b3bd35779f90fe1882e26
nandhuaries <nandhu.s@ariesesolutions.com>
2026-04-13 18:00:31 +0530

    Update meta description and keywords for improved SEO and branding consistency

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta
         name="description"
-        content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website." />
+        content="ATC Solutions - Professional Services for Industrial Applications" />
     <meta
         name="keywords"
-        content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template" />
+        content="ATC Solutions, Industrial Applications, Professional Services" />
     <meta name="author" content="elemis" />
     <title>ATCS</title>
     <link rel="shortcut icon" href="./assets/img/favicon.png" />
